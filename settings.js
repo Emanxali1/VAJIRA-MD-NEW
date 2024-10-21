a970f1d8bb702f0ce344cf47f423d4695c2b0467
Emanxali1 <yyyking77998@outlook.com>
2024-10-21 20:50:56 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=7BkSzbza#DHN-AqhIYT_efzhC1BSm4jYShkYTRVeoynOjdVpE7Ak' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=TMNCnTgA#sHU3iniSi2fTjTI1jh4LOLBl3zhSaLiRJ4fDbGfU2BU' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+923155754954' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
